7fdf65723a0c578eab0e23ff4167be7e9b6e51b3
marcel-alter <h2-o@hotmail.de>
2025-10-30 14:19:39 +0100

    finished Project

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import sys
+import os
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+if not os.path.isfile(book_path):
+    print(f"Error: file not found: {book_path}")
+    sys.exit(2)
+
 def get_book_text(input):
     with open(input) as f:
         file_contents = f.read()
         return file_contents
 
-def main(input_2):
-    book = get_book_text(input_2)
+def main(path):
+    book = get_book_text(path)
     from stats import get_num_words
     num_words = get_num_words(book)
     
@@ -18,7 +29,5 @@ def main(input_2):
     for i in sorted_number:
         print(f"{i["char"]}: {i["num"]}")
 
-
-
-
-main("books/frankenstein.txt")
+print(sys.argv)
+main(sys.argv[1])
